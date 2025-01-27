c492bc9e981165879aa9a361b4e5bfbe9d00f81c
Harkály Gergő <git@harkalygergo.hu>
2025-01-27 19:35:39 +0100

    composer and npm update, plus remove analytics code

--- resources/views/layouts/frontend/default/base.blade.php
+++ resources/views/layouts/frontend/default/base.blade.php
@@ -12,32 +12,6 @@
 
     <link rel="shortcut icon" href="/favicon.ico">
 
-    <!-- check if user is not logged in -->
-    @if (!auth()->check())
-        <meta name="google-site-verification" content="VzCSCU3Byyp98MmggyXqsOrv6u2xrzfA2gqySN0c2Vg" />
-        <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-M8MDVS2G71"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-M8MDVS2G71');
-        </script>
-    @else
-        <!-- if user is logged in, but not an admin -->
-        @if (!auth()->user()->is_admin)
-            <meta name="google-site-verification" content="VzCSCU3Byyp98MmggyXqsOrv6u2xrzfA2gqySN0c2Vg" />
-            <!-- Google tag (gtag.js) -->
-            <script async src="https://www.googletagmanager.com/gtag/js?id=G-M8MDVS2G71"></script>
-            <script>
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-M8MDVS2G71');
-            </script>
-        @endif
-    @endif
-
     <title>{{ env('APP_NAME') }}</title>
 
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
