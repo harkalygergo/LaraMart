cea8e3f70e379d156456362da0c73f0dfbb0c005
Harkály Gergő <git@harkalygergo.hu>
2025-01-27 13:00:43 +0100

    composer and npm update, plus main menu function

--- resources/views/layouts/frontend/default/components/header-forms.blade.php
+++ resources/views/layouts/frontend/default/components/header-forms.blade.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <div class="col-md-7 py-2">
+    <div class="col-md-12 py-2">
         <form method="get" action="/kereses">
             <div class="input-group">
                 <input type="text" name="s" class="form-control" placeholder="Keresés a termékek között" aria-label="Keresés a termékek között" aria-describedby="button-search" value="@isset($searchQuery){{ $searchQuery }}@endisset">
@@ -9,8 +9,5 @@
                 </button>
             </div>
         </form>
-    </div>
-    <div class="col-md-5 py-2">
-        @include('layouts.frontend.default.components.form-imei')
     </div>
 </div>
