3db77add2e8175e09c777c2015a343e5d0abee9a
Harkály Gergő <git@harkalygergo.hu>
2025-02-04 14:41:19 +0100

    composer and npm update, plus remove unnecessary code

--- app/Models/Settings.php
+++ app/Models/Settings.php
@@ -6,7 +6,4 @@ use Illuminate\Database\Eloquent\Model;
 
 class Settings extends Model
 {
-    public $SICKW_API_URL = 'https://sickw.com/api.php?format=beta';
-    public $SICKW_API_USER = 'kovacs.szilvia@pophone.eu';
-    public $SICKW_API_KEY = 'GSP-9MU-HGJ-6RV-2SV-QEC-7ZW-F0N';
 }
