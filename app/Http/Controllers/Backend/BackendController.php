32e90c750033ded30e9c53c2a679a17990d60187
Harkály Gergő <git@harkalygergo.hu>
2025-02-18 09:46:39 +0100

    show settings from database

--- app/Http/Controllers/Backend/BackendController.php
+++ app/Http/Controllers/Backend/BackendController.php
@@ -7,6 +7,7 @@ use App\Models\Ad;
 use App\Models\Attribute;
 use App\Models\Category;
 use App\Models\Merchant;
+use App\Models\Settings;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
@@ -80,6 +81,15 @@ class BackendController extends Controller
             return redirect()->route('showUsers');
         }
 
+        // if it is a delete request, delete the user
+        if (request()->isMethod('delete')) {
+            // delete all ads where user_id is the id
+            Ad::where('user_id', $id)->delete();
+            User::find($id)->delete();
+
+            return redirect()->route('showUsers');
+        }
+
         return view('layouts.backend.editUser', [
             'user' => User::find($id),
         ]);
@@ -87,6 +97,37 @@ class BackendController extends Controller
 
     public function editMerchant($id)
     {
+        // handle the post request
+        if (request()->isMethod('put')) {
+            $merchant = Merchant::find($id);
+            $merchant->name = request('name');
+            $merchant->product_feed_url = request('product_feed_url');
+            $merchant->status = request('status');
+            $merchant->slug = request('slug');
+            $merchant->image = request('image');
+            $merchant->vat = request('vat');
+            $merchant->phone = request('phone');
+            $merchant->email = request('email');
+            $merchant->zip = request('zip');
+            $merchant->city = request('city');
+            $merchant->address = request('address');
+            $merchant->website = request('website');
+            $merchant->description = request('description');
+
+            $merchant->save();
+
+            return redirect()->route('showMerchants');
+        }
+
+        // if it is a delete request, delete the merchant
+        if (request()->isMethod('delete')) {
+            // delete all ads where merchant_id is the id
+            Ad::where('merchant_id', $id)->delete();
+            Merchant::find($id)->delete();
+
+            return redirect()->route('showMerchants');
+        }
+
         return view('layouts.backend.editMerchant', [
             'merchant' => Merchant::find($id),
         ]);
@@ -179,7 +220,34 @@ class BackendController extends Controller
 
     public function showSettings()
     {
-        return view('layouts.backend.settings');
+        // check if the request is a post request
+        if (request()->isMethod('post')) {
+            // get all settings
+            $settings = Settings::all();
+
+            // loop through the settings
+            foreach ($settings as $setting) {
+                // get the setting name
+                $key = $setting->key;
+
+                // get the setting value
+                $value = request($key);
+
+                // update the setting value
+                $setting->value = $value;
+
+                // save the setting
+                $setting->save();
+            }
+
+            return redirect()->route('admin_v1_settings');
+        }
+
+        $settings =Settings::all();
+
+        return view('layouts.backend.settings', [
+            'settings' => $settings
+        ]);
     }
 
     public function showAdmins()
