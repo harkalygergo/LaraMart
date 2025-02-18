fa93926eb3c43f3362765b05ddf7b0a292557399
Harkály Gergő <git@harkalygergo.hu>
2025-02-18 09:10:22 +0100

    settings from database instead of Blade layout file

--- resources/views/layouts/backend/settings.blade.php
+++ resources/views/layouts/backend/settings.blade.php
@@ -2,14 +2,22 @@
 
 @section('main')
     <h2>Beállítások</h2>
-    <ul>
-        <li>BARION_POSKEY_DEMO</li>
-        <li>BARION_LOGIN_DEMO</li>
-        <li>BARION_POSKEY_PROD</li>
-        <li>BARION_LOGIN_PROD</li>
-        <li>Google Maps API Key</li>
-        <li>Számlázzhu</li>
-        <li>Site title</li>
-        <li>Site description</li>
-    </ul>
+
+    <form method="post" action="{{ route('admin_v1_settings') }}">
+        @csrf
+        @foreach ($settings as $setting)
+            <div class="mb-3">
+                <label for="site_title" class="form-label fw-bold">{{ $setting->key }}</label>
+                @if (strlen($setting->value) > 200)
+                    <textarea class="form-control" id="{{ $setting->key }}" name="{{ $setting->key }}" rows="5">
+                        {{ $setting->value }}
+                    </textarea>
+                @else
+                    <input type="text" class="form-control" id="{{ $setting->key }}" name="{{ $setting->key }}" value="{{ $setting->value }}">
+                @endif
+            </div>
+        @endforeach
+        <button type="submit" class="btn btn-primary">Mentés</button>
+    </form>
+
 @endsection
