89c11957ec073dc70b56448bbd6adcad1cd99fed
Harkály Gergő <git@harkalygergo.hu>
2025-02-18 09:37:46 +0100

    repair BlogController

--- app/Http/Controllers/Frontend/BlogController.php
+++ app/Http/Controllers/Frontend/BlogController.php
@@ -7,12 +7,23 @@ use App\Models\Menu;
 
 class BlogController extends Controller
 {
-    public function list()
+    public function index()
     {
-        return view('layouts.frontend.default.pages.list', [
+        return view(env('LAYOUT').'.blog.index', [
             'title' => 'Blog',
-            'ads' => Blog::all(),
+            'blogs' => Blog::all(),
             'menus' => Menu::where('is_active', true)->orderBy('position', 'asc')->get(),
         ]);
+    }
+
+    public function show(string $slug)
+    {
+        $blog = Blog::where('slug', $slug)->first();
+
+        return view(env('LAYOUT').'.page', [
+            'page' => $blog,
+            'menus' => Menu::where('is_active', true)->orderBy('position', 'asc')->get(),
+        ]);
+
     }
 }
