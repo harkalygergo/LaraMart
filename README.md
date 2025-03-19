22182a29c6631c5206d1cb7f302430319a90c264
Harkály Gergő <git@harkalygergo.hu>
2025-03-19 14:03:47 +0100

    rename message user ids to from and to

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # LaraMart | Laravel based marketplace
 
-###### Version: 2025.03.19.5
+###### Version: 2025.03.19.6
 
 LaraMart web application is an online marketplace management tool based on Laravel PHP framework made by @harkalygergo. All rights reserved!
 
--- app/Http/Controllers/Frontend/MessageController.php
+++ app/Http/Controllers/Frontend/MessageController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers\Frontend;
 
+use App\Http\Controllers\Email\EmailController;
 use App\Models\Ad;
 use App\Models\Message;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class MessageController extends Controller
 {
@@ -12,15 +15,46 @@ class MessageController extends Controller
         $loggedInUser = auth()->user();
         $adId = request('ad');
         $ad = Ad::find($adId);
-        $recipientId = is_null($ad->user_id) ? $ad->merchant_id : $ad->user_id;
+
+        if (request('to')) {
+            $recipientId = request('to');
+        } else {
+            $recipientId = is_null($ad->user_id) ? $ad->merchant_id : $ad->user_id;
+        }
 
         $message = new Message();
-        $message->user_id = $loggedInUser->id;
-        $message->recipient_id = $recipientId;
+        $message->from = $loggedInUser->id;
+        $message->to = $recipientId;
         $message->ad_id = $adId;
         $message->message = request('message');
         $message->save();
 
+        // send email to recipient
+        $recipient = User::find($recipientId);
+        (new EmailController())->sendEmail(
+            env('MAIL_FROM_NAME'),
+            env('MAIL_FROM_ADDRESS'),
+            $recipient->name,
+            $recipient->email,
+            'Új üzenet érkezett',
+            request('message').'<br><br><br>'.env('APP_URL'),
+        );
+
         return redirect()->back()->with('success', 'Üzenet elküldve.');
+    }
+
+    public function getMessages()
+    {
+        $userId = Auth::id();
+
+        $groupedMessages = Message::where('from', $userId)
+            ->orWhere('to', $userId)
+            ->orderBy('created_at', 'desc')
+            ->get()
+            ->groupBy(function ($item) {
+                return $item->ad_id . '-' . min($item->from, $item->to) . '-' . max($item->from, $item->to);
+            });
+
+        return $groupedMessages;
     }
 }
