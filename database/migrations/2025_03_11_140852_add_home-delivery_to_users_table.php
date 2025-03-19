350cec8e00d38232d946a7ffd20b4703d0539bd9
Harkály Gergő <git@harkalygergo.hu>
2025-03-19 13:47:24 +0100

    new migration filies

---
+++ database/migrations/2025_03_11_140852_add_home-delivery_to_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // add home_delivery column
+            $table->boolean('home_delivery')->default(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // drop home_delivery column
+            $table->dropColumn('home_delivery');
+        });
+    }
+};
